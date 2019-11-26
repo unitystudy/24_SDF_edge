30fa13541fbfd5b8ee1b5a51bc290f63d5907eb8
imagire <imagire@gmail.com>
2019-11-26 21:35:18 +0900

    一通りの流れはできた

--- src/make_sdf/make_sdf.cpp
+++ src/make_sdf/make_sdf.cpp
@@ -4,7 +4,8 @@
 //  filename: 入力色ファイル名
 //  -o:  出力ファイル名（色）
 //  -s:  出力ファイル名（SDF）
-//  -h:  縮小率を指定。ひとまず元の解像度の1/2のべき乗を期待
+//  -h:  出力縦幅。ひとまず縦横元の解像度の1/2のべき乗を期待
+//  -t:  あるなしを判断するアルファ値の閾値
 
 #include "pch.h"
 #include <iostream>
@@ -20,6 +21,7 @@ struct INIT_DATA{
 	const char *filename;// 入力ファイル名
 	const char *out_sdf;// 出力SDFファイル名
 	const char *out_rgba;// 出力色ファイル名
+	unsigned char threshhold;// あるなしを判断するアルファ値の閾値
 };
 
 struct RGBA8 {
@@ -28,6 +30,14 @@ struct RGBA8 {
 struct RGBA32F {
 	float r, g, b, a;
 };
+
+// l以上で最も小さい2のべき乗の数
+unsigned int get_level_of_power_of_2(unsigned int l)
+{
+	unsigned int po = 0;
+	while ( (1u << po) < l ) po++;
+	return po;
+}
 
 float col2float(unsigned char c) {
 	return (1.0f/255.0f)*(float)c;
@@ -92,6 +102,133 @@ void reduction(
 	}
 }
 
+// 2D空間のモートン番号を算出
+unsigned int get_morton_index(unsigned int x, unsigned int y)
+{
+	auto  bit_separate = [](unsigned int n){
+		n = (n | (n << 8)) & 0x00ff00ff;
+		n = (n | (n << 4)) & 0x0f0f0f0f;
+		n = (n | (n << 2)) & 0x33333333;
+		n = (n | (n << 1)) & 0x55555555;
+		return n;
+	};
+	return (bit_separate(x) | (bit_separate(y) << 1));
+}
+
+void morton_order(unsigned char *dest, const RGBA8 *src, unsigned int w, unsigned int h, unsigned int size)
+{
+	for (unsigned int y = 0; y < h; y++) {
+		for (unsigned int x = 0; x < w; x++) {
+			dest[get_morton_index(x, y)] = src[x + y * w].a;
+		}
+		// 右側あまり
+		for (unsigned int x = w; x < size; x++) {
+			dest[get_morton_index(x, y)] = 0;
+		}
+	}
+	// 下側あまり
+	for (unsigned int y = h; y < size; y++) {
+		for (unsigned int x = 0; x < size; x++) {
+			dest[get_morton_index(x, y)] = 0;
+		}
+	}
+}
+// 最大値での hierarchy 計算
+void morton_order_hierarchy(unsigned char *dest, const unsigned char *src, unsigned int size)
+{
+	for (unsigned int idx = 0; idx < size * size; idx++) {
+		const unsigned char *p = src + (idx << 2);
+		unsigned char a0 = p[0];
+		unsigned char a1 = p[1];
+		unsigned char a2 = p[2];
+		unsigned char a3 = p[3];
+		unsigned char b0 = (a0 < a1) ? a1 : a0;
+		unsigned char b1 = (a2 < a3) ? a3 : a2;
+		dest[idx] = (b0 < b1) ? b1 : b0;
+		p += 4;
+	}
+}
+
+#define MAX(a,b) (((a)<(b))?(b):(a))
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+struct bbox{
+	unsigned int min[2];
+	unsigned int max[2];
+};
+
+unsigned int get_max_distance(const bbox b, unsigned int x, unsigned int y)
+{
+	unsigned int dx0 = (b.max[0] - x)*(b.max[0] - x);
+	unsigned int dx1 = (b.min[0] - x)*(b.min[0] - x);
+	unsigned int dy0 = (b.max[1] - y)*(b.max[1] - y);
+	unsigned int dy1 = (b.min[1] - y)*(b.min[1] - y);
+
+	return ((dx0 < dx1) ? dx1 : dx0) + ((dy0 < dy1) ? dy1 : dy0);
+}
+
+unsigned int get_distance(unsigned int idx, unsigned int level, const bbox &bb, unsigned int src_x, unsigned int src_y, unsigned char **a_morton_ordered_a, unsigned char threshhold)
+{
+	// データのない領域
+	const unsigned char *morton_ordered_a = a_morton_ordered_a[level];
+	if (morton_ordered_a[idx] < threshhold) return 0xffffffff;
+
+	// そこにデータはある
+	if (level == 0) return 0;
+
+	// 何かあるので、子供をさらに探索
+	unsigned int distance = 0xffffffff;
+	idx *= 4;
+	unsigned int center[2] = {(bb.min[0]+bb.max[0])/2, (bb.min[0]+bb.max[1])/ 2 };
+	bbox b0 = { {bb.min[0], bb.min[1]},{center[0], center[1]} };
+	if (get_max_distance(b0, src_x, src_y) < distance) 
+	{
+		unsigned int d = get_distance(idx + 0, level - 1, b0, src_x, src_y, a_morton_ordered_a, threshhold);
+		distance = MIN(distance, d);
+	}
+	bbox b1 = { {center[0], bb.min[1]},{bb.max[0], center[1]} };
+	if (get_max_distance(b1, src_x, src_y) < distance) 
+	{
+		unsigned int d = get_distance(idx + 1, level - 1, b1, src_x, src_y, a_morton_ordered_a, threshhold);
+		distance = MIN(distance, d);
+	}
+	bbox b2 = { {bb.min[0], center[1]},{center[0], bb.max[1]} };
+	if (get_max_distance(b2, src_x, src_y) < distance) 
+	{
+		unsigned int d = get_distance(idx + 2, level - 1, b2, src_x, src_y, a_morton_ordered_a, threshhold);
+		distance = MIN(distance, d);
+	}
+	bbox b3 = { {center[0], center[1]},{bb.max[0], bb.max[1]} };
+	if (get_max_distance(b3, src_x, src_y) < distance) 
+	{
+		unsigned int d = get_distance(idx + 3, level - 1, b3, src_x, src_y, a_morton_ordered_a, threshhold);
+		distance = MIN(distance, d);
+	}
+
+	return distance;
+}
+
+void generate_sdf(RGBA8 *dest, unsigned int dw, unsigned int dh, unsigned char **a_morton_ordered_a, unsigned int max_level, unsigned int sw, unsigned int sh, unsigned char threshhold)
+{
+	for (unsigned int y = 0; y < dh; y++) {
+		for (unsigned int x = 0; x < dw; x++) {
+			unsigned int src_x = (x * sw + (sw >> 1)) / dw;// ピクセル中心の元の画像の位置
+			unsigned int src_y = (y * sh + (sh >> 1)) / dh;
+
+			// (src_x, src_y から最も近い位置を見つけていく)
+			unsigned int size = 1u << max_level;
+			bbox bb = { {0, 0}, {size, size} };
+			float distance = sqrt((float)get_distance(0, max_level, bb, src_x, src_y, a_morton_ordered_a, threshhold));
+
+			RGBA8 &c = dest[y * dw + x];
+			c.a = (unsigned char)(MAX(0.0f, MIN(255.999999f, distance + 128.0f)));// -128から127で0.5オフセットで正値化
+			c.r = (unsigned char)(MAX(0.0f, MIN(255.999999f, distance)));// 正の値で記録
+			c.g = (unsigned char)(MAX(0.0f, MIN(255.999999f,-distance)));// 負の値で記録
+			c.b = (0 <= distance) ? 0 : 255;// 正負
+		}
+	}
+}
+
 void make_sdf(const INIT_DATA &init)
 {
 	// load data
@@ -102,30 +239,45 @@ void make_sdf(const INIT_DATA &init)
 	if (image == nullptr) throw(std::string("Failed to load texture"));
 	if (channels != 4) throw(std::string("No alpha image"));// need RGBA channel
 
-	// create z-order memory image
-
-	// generate mips
-
-	// 縮小イメージの作成
+	// create reduction color image
 	int sw = init.out_height * w / h;
 	int sh = init.out_height;
 	RGBA8 *img_rgba = new RGBA8[sw * sh];
-	RGBA8 *img_sdf = new RGBA8[sw * sh];
-
-
-	// create SDF
-
-	// create reduction color image
 	reduction(image, w, h, (unsigned char*)img_rgba, sw, sh);
 
-	// save data
+	// save reduction color image
 	int stride_in_bytes = sw * 4;
 	stbi_write_png(init.out_rgba, sw, sh, STBI_rgb_alpha, (unsigned char*)img_rgba, stride_in_bytes);
+	delete[] img_rgba;
+
+	// create Morton order alpha image
+	unsigned int lx = get_level_of_power_of_2(w);
+	unsigned int ly = get_level_of_power_of_2(h);
+	unsigned int level = (lx < ly) ? ly : lx;// max
+	unsigned int size2 = 1u << level;// w,h以上で最小の2のべき乗のサイズ
+	unsigned char **a_morton_ordered_a = new unsigned char *[level+1];
+
+	a_morton_ordered_a[0] = new unsigned char[size2 * size2];
+	morton_order(a_morton_ordered_a[0], (const RGBA8 *)image, w, h, size2);
+
+	// create Morton order alpha image hierarchy
+	for (unsigned int i = 1; i <= level; i++) {
+		size2 >>= 1;
+		a_morton_ordered_a[i] = new unsigned char[size2 * size2];
+		morton_order_hierarchy(a_morton_ordered_a[i], a_morton_ordered_a[i-1], size2);
+	}
+
+	// create SDF
+	RGBA8 *img_sdf = new RGBA8[sw * sh];
+	generate_sdf(img_sdf, sw, sh, a_morton_ordered_a, level, w, h, init.threshhold);
 	stbi_write_png(init.out_sdf, sw, sh, STBI_rgb_alpha, (unsigned char*)img_sdf, stride_in_bytes);
 
-	// 解法
-	delete[] img_rgba;
+	// release
 	delete[] img_sdf;
+	for (unsigned int i = 0; i <= level; i++) {
+		delete[] a_morton_ordered_a[i];
+	}
+	delete[] a_morton_ordered_a;
 	stbi_image_free(image);
 }
 
@@ -137,6 +289,7 @@ int main(int argc, char *argv[])
 	src.filename = "src.png";
 	src.out_sdf  = "sdf.png";
 	src.out_rgba = "dest.png";
+	src.threshhold = 255;
 
 	for (int i = 1; i < argc; i++) {
 		switch (argv[i][0]) {
@@ -150,6 +303,9 @@ int main(int argc, char *argv[])
 				break;
 			case 'h':
 				src.out_height = atoi(argv[++i]);
+				break;
+			case 't':
+				src.threshhold = atoi(argv[++i]);
 				break;
 			default:
 				break;
@@ -165,14 +321,3 @@ int main(int argc, char *argv[])
 
     return 0; 
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
